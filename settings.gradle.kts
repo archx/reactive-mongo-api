3472aeee7a802fd3b112c5db5292513b23727b42
Archx <archx@foxmail.com>
2020-10-15 16:37:09 +0800

    update gradle plugins repositories

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,9 @@
 rootProject.name = "reactive-mongo-api"
+pluginManagement {
+    repositories {
+        maven {
+            url = uri("https://maven.aliyun.com/repository/gradle-plugin")
+        }
+        gradlePluginPortal()
+    }
+}
